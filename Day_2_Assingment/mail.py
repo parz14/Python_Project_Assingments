9a0d71b38d38efe105b9d9d95edb8528a4b1d318
parz14 <62294775+parz14@users.noreply.github.com>
2021-09-04 14:30:48 +0530

    Delete Day_2_Assingment directory

--- Day_2_Assingment/mail.py
+++
@@ -1,9 +0,0 @@
-import re
-FILE = open("mail.txt", "r+")
-next(FILE)
-for mail_ids in FILE:
-    if re.match("[a-z A-Z 0-9]+@gmail.com$", mail_ids):
-        print("Valid")
-    else:
-        print("Not valid")
-FILE.close()
